50f1082cb13aa4779ca7f23489abefdc92876f68
GalloFrancesco01 <francescogallo00@icloud.com>
2024-04-11 18:00:51 +0200

    inzio

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
-const container = document.getElementById("container");
-const link = document.createElement("a");
-
-data.forEach((object) => {
-  link.href = object.gameUrl;
-  link.innerText = object.name;
-  container.appendChild(link);
-})
+const btn = document.getElementById('btn');
+let randomNum = () => {
+  return Math.floor(Math.random()  * 256);
+};
+let chengeColor = () => {
+  let randomColor = `rgb(${randomNum()},$ 
+{randomNum()},
+}
