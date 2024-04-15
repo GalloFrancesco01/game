f6dffe941d2f4fd6af81f631f19febd3c93974fe
GalloFrancesco01 <francescogallo00@icloud.com>
2024-04-15 16:25:05 +0200

    change color

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
-const btn = document.getElementById('btn');
-let randomNum = () => {
-  return Math.floor(Math.random()  * 256);
-};
-let chengeColor = () => {
-  let randomColor = `rgb(${randomNum()},$ 
-{randomNum()},
-}
+const body= document.querySelector('body');
+const button= document.querySelector('button');
+const color =['red', 'green','violet', 'black', 'yellow', 'pink', 'brown', 'white', 'orange'];
+
+body.style.backgroundColor = 'white';
+button.addEventListener('click', function(){
+const colorIndex = parseInt(Math.random()*color.length);
+body.style.backgroundColor=color[colorIndex];
+})
+
+
+
+
+
